d68f39b7b0ed797ca5295c6968dedf058a339c35
chinthakaak <chinthakaak@gmail.com>
2015-12-11 13:33:30 +0530

    Initial commit

---
+++ src/main/java/com/examples/Main.java
@@ -0,0 +1,10 @@
+package com.examples;
+
+/**
+ * Created by ka40215 on 12/11/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main");
+    }
+}
